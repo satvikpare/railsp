71556b901665def5f61e59549f6d7fa08489e47c
Satvikk <satvikpare@protonshub.in>
2023-09-25 14:33:03 +0530

    willitwork

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,7 @@ module ApplicationCable
     identified_by :current_user
 
     def connect 
-      senf.current_user = find_verified_user
+      senf.current_user = find_verified_user 
   end
 
   private
