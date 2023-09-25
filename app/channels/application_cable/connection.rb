e79fb495337d67ebe56b9c97d3d527e1e6a28199
Satvikk <satvikpare@protonshub.in>
2023-09-25 18:22:15 +0530

    some changes

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,20 +3,21 @@ module ApplicationCable
     identified_by :current_user
     rescue_from StandardError, with: :report_error
     def connect 
-      senf.current_user = find_verified_user
-  end
-
-  private
-  def find_verified_user
-    if verified_user = User.fimd_by(id: cookies.encrypted['_session_id']['user_id'])
-      verified_user
-    else
-      reject_unauthorized_connection
+      # send.current_user = find_verified_user
     end
-  end
 
-  def report_error(e)
-    SomeExternalBugtrackingService.notify(e)
+    private
+    def find_verified_user
+      if verified_user = User.find_by(id: cookies.encrypted['_session_id']['user_id'])
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
+    def report_error(e)
+      SomeExternalBugtrackingService.notify(e)
+    end
   end
 end
 
